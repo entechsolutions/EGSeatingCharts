17b5c4bad27a05244525686ac3faccb730b581c8
Danila Parkhomenko <thereaintnomailformeagain@gmail.com>
2017-01-25 21:51:51 +0300

    Initial Commit

---
+++ EGSeatingChartsExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EGSeatingChartsExample
+//
+//  Created by Danila Parkhomenko on 25/01/2017.
+//  Copyright © 2017 ENtechsolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EGSeatingChartsExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EGSeatingChartsExample
+//
+//  Created by Danila Parkhomenko on 25/01/2017.
+//  Copyright © 2017 ENtechsolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
